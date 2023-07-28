33f8a83276af3b61cd675f9913afa6006e40ffcb
oleg.gusev <oleg.gusev@gmail.com>
2023-07-28 11:48:58 +0300

    updated django

--- core/admin.py
+++ core/admin.py
@@ -6,9 +6,18 @@ from core.models import UserInfo
 # Register your models here.
 class UserInfoAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'second_name')
-    readonly_fields = ['User_photo']
+    readonly_fields = ['User_preview']
     list_display = ('first_name', 'second_name', 'dob')
-    exclude = ['upload', "User_photo"]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['User_photo']
+        return []
+
+    def get_exclude(self, request, obj=None):
+        if obj:
+            return ['upload']
+        return []
 
 
 admin.site.register(UserInfo, UserInfoAdmin)
